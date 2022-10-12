51d157ea9c4419e1eeae3308aac439ca2a4e69f4
victorytory <victorytory01398@gmail.com>
2022-10-12 14:02:11 +0300

    js logic for counter

--- js/counter-02.js
+++ js/counter-02.js
@@ -17,6 +17,17 @@ window.addEventListener('click', function (event) {
 
         if(parseInt(counter.innerText) > 1) {
             counter.innerText = --counter.innerText;
+        } else if (event.target.closest('.cart-wrapper') && parseInt(counter.innerText) === 1) {
+            // checking product in basket
+            event.target.closest('.cart-item').remove();
+
+            toggleCartStatus();
+
+            calcCartPriceAndDelivery();
         }
+    }
+
+    if (event.target.hasAttribute('data-action') && event.target.closest('.cart-wrapper')) {
+        calcCartPriceAndDelivery();
     }
 });
